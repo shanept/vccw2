9a2358b2af00a954602e79403910121ad1b9bd72
Shane Thompson <me@shane.pt>
2024-04-16 00:14:44 +0800

    Added wp_debug_log and wp_debug_display site configuration variables

--- provision/roles/wordpress/templates/extra-wp-config.php
+++ provision/roles/wordpress/templates/extra-wp-config.php
@@ -1,5 +1,7 @@
 define( 'JETPACK_DEV_DEBUG', {{ vccw.wp_debug }} );
 define( 'WP_DEBUG', {{ vccw.wp_debug }} );
+define( 'WP_DEBUG_LOG', {{ vccw.wp_debug_log }} );
+define( 'WP_DEBUG_DISPLAY', {{ vccw.wp_debug_display }} );
 define( 'FORCE_SSL_ADMIN', {{ vccw.force_ssl_admin }} );
 define( 'SAVEQUERIES', {{ vccw.savequeries }} );
 
